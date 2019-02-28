f2500f8247f055bc49645eaeb692c5e4f87ffe2d
matheusps <matheus.ps@icloud.com>
2019-02-28 15:14:53 -0300

    Change constants for defaultProps

--- react/components/SearchBar/index.js
+++ react/components/SearchBar/index.js
@@ -1,30 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import SearchBar from './components/SearchBar'
 import { injectIntl, intlShape } from 'react-intl'
 
-const SEARCH_DELAY_TIME = 500
-
 /** Canonical search bar that uses the autocomplete endpoint to search for a specific product*/
 const SearchBarContainer = (
-  { intl, compactMode, hasIconLeft, iconClasses }, //props
+  { intl, compactMode, hasIconLeft, iconClasses, searchDelay }, //props
   context
 ) => {
-  let timeoutId = null
   const [shouldSearch, setShouldSearch] = useState(false)
   const [inputValue, setInputValue] = useState('')
 
-  const handleClearSearchResults = () => {
-    setShouldSearch(false)
-    timeoutId = null
-  }
-
-  const handleClearInput = () => {
+  const handleClearInput = useCallback(() => {
     setInputValue('')
-    handleClearSearchResults()
-  }
+    setShouldSearch(false)
+  }, [])
 
-  const handleGoToSearchPage = () => {
+  const handleGoToSearchPage = useCallback(() => {
     const search = inputValue
     setInputValue('')
     context.navigate({
@@ -33,23 +25,25 @@ const SearchBarContainer = (
       query: 'map=ft',
       fallbackToWindowLocation: false,
     })
-  }
+  }, [])
 
-  const handleEnterPress = event =>
-    event.key === 'Enter' && handleGoToSearchPage()
+  const handleEnterPress = useCallback(
+    event => event.key === 'Enter' && handleGoToSearchPage(),
+    []
+  )
 
-  const handleMakeSearch = () => {
-    handleClearSearchResults()
-    timeoutId = setTimeout(() => {
+  const handleMakeSearch = useCallback(() => {
+    setShouldSearch(false)
+    setTimeout(() => {
       setShouldSearch(true)
-    }, SEARCH_DELAY_TIME)
-  }
+    }, searchDelay)
+  }, [])
 
-  const handleInputChange = event => {
+  const handleInputChange = useCallback(event => {
     const value = event.target.value
     setInputValue(value)
-    value.length < 2 ? handleClearSearchResults() : handleMakeSearch()
-  }
+    value.length < 2 ? setShouldSearch(false) : handleMakeSearch()
+  }, [])
 
   const placeholder = intl.formatMessage({
     id: 'search.placeholder',
@@ -76,6 +70,10 @@ const SearchBarContainer = (
   )
 }
 
+SearchBarContainer.defaultProps = {
+  searchDelay: 500,
+}
+
 SearchBarContainer.contextTypes = {
   navigate: PropTypes.func,
 }
@@ -89,6 +87,8 @@ SearchBarContainer.propTypes = {
   hasIconLeft: PropTypes.bool,
   /** Custom classes for the search icon */
   iconClasses: PropTypes.string,
+  /** Delay of search foreach interaction, in ms */
+  searchDelay: PropTypes.number,
 }
 
 export default injectIntl(SearchBarContainer)
